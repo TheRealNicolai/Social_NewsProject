c075797e92817f92bf6450e99109200cb48a976e
TheRealNicolai <nicolaimunksgaardschmidt@gmail.com>
2025-11-05 14:12:17 +0100

    Creating cosine similarity function, and cosine similarity pickle-files for titles and descriptions. Turning embedding into a function. Creating a main project notebook.

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,13 +1,34 @@
 import numpy as np
 from numpy.linalg import norm
-
-input_data = np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]])
-epsilon = 1e-10
+import pickle
 
 def cosine_sim(input_data, epsilon=1e-10):
+    # Checking if numpy array
+    if not isinstance(input_data, np.ndarray):
+        input_data = np.array(input_data)
+    # Calculating cosine similarities for all rows with one another.
     cos_sim = np.zeros((input_data.shape[0], input_data.shape[0]))
-    for row in input_data:
-        for row2 in input_data:
+    for i, row in enumerate(input_data):
+        for j, row2 in enumerate(input_data):
             cos_data = np.dot(row, row2) / (norm(row) * norm(row2) + epsilon)
-            cos_sim[input_data.tolist().index(row)][input_data.tolist().index(row2)] = cos_data
+            cos_sim[i][j] = cos_data
     return cos_sim
+
+if __name__ == '__main__':
+    with open('embedded_articles.pkl', 'rb') as f:
+        embedded_articles = pickle.load(f)
+    
+    emb_titles = []
+    emb_desc = []
+    for key in embedded_articles.keys():
+        emb_titles.append(embedded_articles[key]['embedded_title'])
+        emb_desc.append(embedded_articles[key]['embedded_description'])
+    
+    cossim_title = cosine_sim(emb_titles)
+    cossim_desc = cosine_sim(emb_desc)
+
+    with open('cosine_similarities_titles.pkl', 'wb') as f:
+        pickle.dump(cossim_title, f)
+
+    with open('cosine_similarities_descriptions.pkl', 'wb') as f:
+        pickle.dump(cossim_desc, f)
